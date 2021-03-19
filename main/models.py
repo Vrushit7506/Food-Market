f55afa83c01aef89ac23f7d04f6e5398d3304143
Vrushit7506 <vrushit7506@gmail.com>
2021-03-19 13:49:11 +0530

    Register App Added

--- main/models.py
+++ main/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class ChatDB(models.Model):
+  user = models.CharField(max_length=200, blank=True)
+  text = models.TextField(blank=True)
+  className = models.CharField(max_length=200, blank=True)
+  chatno = models.AutoField(primary_key=True)
+
+  def __str__(self):
+        return self.user + " - " + str(self.chatno)
