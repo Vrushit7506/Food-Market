a2fb74fe5becada0940077a7a2cb8bd87d8de1f4
Vrushit7506 <vrushit7506@gmail.com>
2021-04-08 02:28:06 +0530

    Heroku Changes 3

--- Food_Stock_Exchange/settings.py
+++ Food_Stock_Exchange/settings.py
@@ -21,8 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = '9d^8y45m#1g!(nc*7i3rb$_b(-$^$=jha0xil&_mbt$44o@wq+'
-SECRET_KEY = os.environ.get('BSE_SECRET_KEY')
+SECRET_KEY = '9d^8y45m#1g!(nc*7i3rb$_b(-$^$=jha0xil&_mbt$44o@wq+'
+# SECRET_KEY = os.environ.get('BSE_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
