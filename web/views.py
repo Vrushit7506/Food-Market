121d66b4e322c0be18e41201c4086bc64cb3b0a8
Vrushit7506 <vrushit7506@gmail.com>
2021-04-09 13:36:56 +0530

    Final Push 1

--- web/views.py
+++ web/views.py
@@ -10,13 +10,46 @@ def project(request):
     return render(request, "web/index.html")
 
 def stock(request):
+    all_drinktype = (
+        ("1", "Beer"),
+        ("2", "Cocktail"),
+        ("3", "Gin"),
+        ("4", "Red Wine"),
+        ("5", "Sparkling Wine"),
+        ("6", "Vodka"),
+        ("7", "Whiskey"),
+        ("8", "White Wine"),
+    )
+
     all_drinks = barMenu.objects.all().order_by('drinktype')
+
+    for i in all_drinks:
+        number = int(i.drinktype) - 1
+        i.drinktype = all_drinktype[number][1]
+
+    
     return render(request, "web/stock.html", {"all_drinks": all_drinks})
 
 
 def stock_load_ajax(request):
+    all_drinktype = (
+        ("1", "Beer"),
+        ("2", "Cocktail"),
+        ("3", "Gin"),
+        ("4", "Red Wine"),
+        ("5", "Sparkling Wine"),
+        ("6", "Vodka"),
+        ("7", "Whiskey"),
+        ("8", "White Wine"),
+    )
     if request.method == 'GET':
         print("------Check----------")
-        all_drinks = serializers.serialize("json", barMenu.objects.all().order_by('drinktype'))
+        all_drinks = barMenu.objects.all().order_by('drinktype')
+
+        for i in all_drinks:
+            number = int(i.drinktype) - 1
+            i.drinktype = all_drinktype[number][1]
+
+        all_drinks = serializers.serialize("json", all_drinks)
         all_drinks = json.loads(all_drinks)
         return JsonResponse(all_drinks, safe=False)
