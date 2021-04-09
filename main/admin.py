e621961e7340415d713d374e7389404158f429ac
Vrushit7506 <vrushit7506@gmail.com>
2021-04-09 11:45:05 +0530

    Order Changes

--- main/admin.py
+++ main/admin.py
@@ -4,15 +4,18 @@ from .models import foodMenu
 from .models import barMenu
 from .models import ordered
 from import_export.admin import ImportExportModelAdmin
-from .resources import barMenuResource
+from .resources import barMenuResource, foodMenuResource
 
 # Register your models here.
 
 class barMenuAdmin(ImportExportModelAdmin):
     resource_class = barMenuResource
 
+class foodMenuAdmin(ImportExportModelAdmin):
+    resource_class = foodMenuResource
+
 admin.site.register(Message)
-admin.site.register(foodMenu)
-admin.site.register(barMenu)
+admin.site.register(foodMenu, ImportExportModelAdmin)
+admin.site.register(barMenu, ImportExportModelAdmin)
 admin.site.register(ordered)
 
--- main/resources.py
+++ main/resources.py
@@ -1,6 +1,11 @@
 from .models import barMenu
+from .models import foodMenu
 from import_export import resources
 
 class barMenuResource(resources.ModelResource):
     class Meta:
         model = barMenu
+
+class foodMenuResource(resources.ModelResource):
+    class Meta:
+        model = foodMenu
